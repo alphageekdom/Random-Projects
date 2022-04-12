0eae70157c252998bfbd92a7acd2fc43f48fb03b
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-04-12 11:16:53 -0700

    New snake project

---
+++ snake_game/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import time
+import random
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Solid 🐍 Game")
+
+
+def create_snake():
+    
+
+
+screen.exitonclick()
