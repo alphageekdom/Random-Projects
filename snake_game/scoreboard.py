ba877f3c140e1a46b3e1ae1ac85b278ad8c9ed67
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-04-13 17:05:47 -0700

    update

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -17,14 +17,17 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        """Displays scoreboard"""
         self.score_num = f"Score: {self.score}"
         self.write(arg=self.score_num, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
+        """Increases the score"""
         self.clear()
         self.score += 1
         self.update_scoreboard()
 
     def game_over(self):
+        """Displays 'GAME OVER' in the middle of the screen"""
         self.goto(x=0, y=0)
         self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
