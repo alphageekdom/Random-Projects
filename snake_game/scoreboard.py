b46b3a8b27bd9d15ef94e507f929c478143c417c
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-04-16 09:20:59 -0700

    update

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", "r") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(x=0, y=270)
@@ -17,16 +19,23 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         """Displays scoreboard"""
-        self.score_num = f"Score: {self.score}"
-        self.write(arg=self.score_num, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}     High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+    
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "a") as data:
+                data.write(f"\n{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         """Increases the score"""
-        self.clear()
         self.score += 1
         self.update_scoreboard()
-
-    def game_over(self):
-        """Displays 'GAME OVER' in the middle of the screen"""
-        self.goto(x=0, y=0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    
+    #def game_over(self):
+    #    """Displays 'GAME OVER' in the middle of the screen"""
+    #    self.goto(x=0, y=0)
+    #    self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -31,6 +31,13 @@ class Snake:
     def extend(self):
         """Increases the length of the snake"""
         self.add_segment(self.segments[-1].position())
+    
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.make_snake()
+        self.head = self.segments[0]
 
     def move(self):
         """Givers movement to the snake"""
