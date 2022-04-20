96e8718522297a937b261e9c30a4efe287f8fe38
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-04-20 16:50:11 -0700

    update

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -14,6 +14,15 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(
         title=f"{len(guessed_states)}/50 States Correct", prompt="         Name a state:").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+        
     if answer_state in all_states:
         t = turtle.Turtle()
         t.hideturtle()
@@ -22,5 +31,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         guessed_states.append(answer_state)
-
-screen.exitonclick()
