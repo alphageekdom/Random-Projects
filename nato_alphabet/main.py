f4b363a337d735a4497457ccba4bbe49a460660a
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-05-20 15:25:04 -0700

    update with function

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -3,11 +3,14 @@ import pandas
 data = pandas.read_csv("./nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-while True:
+def generate_phonetic():
     word = input("Enter a word: ").upper()
     try:
         output = [phonetic_dict[letter] for letter in word]
     except KeyError:
         print("Sorry, only letters!")
+        generate_phonetic()
     else:
         print(output)
+
+generate_phonetic()
