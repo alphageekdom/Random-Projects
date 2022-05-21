450090b7bc6f7954d0048cc873d59c5d613384bb
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-05-21 12:43:19 -0700

    update

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,7 +1,8 @@
-from tkinter import *
-from tkinter import messagebox
 from random import choice, randint, shuffle
+from tkinter import messagebox
+from tkinter import *
 import pyperclip
+import json
 
 WHITE = "white"
 BLACK = "black"
@@ -11,7 +12,7 @@ BLACK = "black"
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -33,24 +34,61 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(
             title="Oops", message="Don't leave any field empty!")
 
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"Details entered: \nEmail: {email}"
-                                    f"\nPassword: {password} \nIs it ok to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                # Read old data
+                data = json.load(data_file)
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+
+        else:
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="ERROR", message="No Data File Found.")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(
+                title="ERROR", message=f"No details for the {website} exists.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 # Window configuration
 window = Tk()
 window.title("Password Generator 🔐")
@@ -74,28 +112,32 @@ password_label = Label(text="Password:", bg=WHITE, fg=BLACK)
 password_label.grid(column=0, row=3)
 
 # Entries
-website_entry = Entry(width=40, bg=WHITE, fg=BLACK,
-                    highlightthickness=0, insertbackground=BLACK)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=24, bg=WHITE, fg=BLACK,
+                      highlightthickness=0, insertbackground=BLACK)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
-email_entry = Entry(width=40, bg=WHITE, fg=BLACK,
+email_entry = Entry(width=38, bg=WHITE, fg=BLACK,
                     highlightthickness=0, insertbackground=BLACK)
-email_entry.insert(0, "alphageekdom@gmail.com")
+email_entry.insert(0, "mytestemail@gmail.com")
 email_entry.grid(column=1, row=2, columnspan=2)
 
-password_entry = Entry(width=22, bg=WHITE, fg=BLACK,
-                    highlightthickness=0, insertbackground=BLACK)
+password_entry = Entry(width=24, bg=WHITE, fg=BLACK,
+                       highlightthickness=0, insertbackground=BLACK)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-generate_button = Button(text="Generate Password",
-                        highlightbackground=WHITE, command=generate_password)
+generate_button = Button(text="Generate Password", width=10,
+                         highlightbackground=WHITE, command=generate_password)
 generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36,
                     highlightbackground=WHITE, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", width=10,
+                       highlightbackground=WHITE, command=find_password)
+search_button.grid(column=2, row=1)
 
 
 window.mainloop()
